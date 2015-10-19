b929cb1919d1752646cb10f8fa79b7daa2b5d9c0
evan <eg1290@gmail.com>
2015-10-19 09:56:26 -0400

    Bottles v2, with helper methods

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -1,16 +1,23 @@
 class Bottles
     def verse(n)
-        if n == 0
-            return "No more bottles of beer on the wall, " +
-            "no more bottles of beer.\n" +
-            "Go to the store and buy some more, " +
-            "99 bottles of beer on the wall.\n"
-        end
-
-        "#{n} bottle#{'s' if n > 1} of beer on the wall, " +
-        "#{n} bottle#{'s' if n > 1} of beer.\n" +
-        "Take #{(n > 1) ? 'one' : 'it'} down and pass it around, " +
-        "#{(n > 1) ?  n-1 : 'no more'} bottle#{'s' if n-1 != 1} of beer on the wall.\n"
+        bottle = pluralize('bottle', n)
+        "#{non_zero(n)} #{bottle} of beer on the wall, ".capitalize +
+        "#{non_zero(n)} #{bottle} of beer.\n" +
+        zero_switch("Take #{plural_switch('one', 'it', n)} down and pass it around, ",
+                    "Go to the store and buy some more, ", n) +
+        "#{non_zero((n-1) % 100)} #{pluralize('bottle', (n-1) % 100)} of beer on the wall.\n"
+    end
+    def non_zero(n)
+       zero_switch(n, 'no more', n)
+    end
+    def zero_switch(str1, str2, n)
+        (n == 0) ? str2 : str1
+   end
+    def pluralize(str, n)
+        plural_switch(str+'s', str, n)
+    end
+    def plural_switch(str1, str2, n)
+        (n != 1) ? str1 : str2
     end
     def verses(start, stop)
         (stop..start).map { |v|
